111d1cbdd8f7e89bf2d475416b5f9dd5e5840139
byron bess <byronbess@knights.ucf.edu>
2021-03-14 22:21:44 -0400

    First commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,17 +7,94 @@ public class PlayerScript : MonoBehaviour
 {
     private Rigidbody2D rd2d;
 
+    private bool isOnGround;
+
+    private bool facingRight = true;
+
+    public Transform groundcheck;
+
+    public float checkRadius;
+
+    public LayerMask allGround;
+
     public float speed;
+
+    public float jumpForce;
 
     public Text score;
 
+    public Text win;
+
+    public Text livesText;
+
+    private int lives;
+
     private int scoreValue = 0;
+
+    public AudioSource musicSource;
+
+    public AudioClip musicClipOne;
+
+    public AudioClip musicClipTwo;
+
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
+        anim = GetComponent<Animator>();
+        win.text = "";
+        lives = 3;
+        SetlivesText();
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetInteger("State", 2);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+
     }
 
     // Update is called once per frame
@@ -26,6 +103,18 @@ public class PlayerScript : MonoBehaviour
         float hozMovement = Input.GetAxis("Horizontal");
         float vertMovement = Input.GetAxis("Vertical");
         rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+        isOnGround = Physics2D.OverlapCircle(groundcheck.position, checkRadius, allGround);
+
+        if (facingRight == false && hozMovement > 0)
+        {
+            Flip();
+
+        }
+
+        else if (facingRight == true && hozMovement < 0)
+        {
+            Flip();
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -35,17 +124,46 @@ public class PlayerScript : MonoBehaviour
             scoreValue += 1;
             score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+            SetlivesText();
+        }
+        else if (collision.collider.tag == "Enemy")
+        {
+            Destroy(collision.collider.gameObject);
+            lives = lives - 1;
+            SetlivesText();
         }
 
+        if (scoreValue == 4)
+        {
+            transform.position = new Vector3(55.0f, 1.0f);
+        }
+
+        if (scoreValue >= 8)
+        {
+            win.text = "You win! Game created by Byron Bess!";
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+
+        }
+    }
+
+    void SetlivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+        if (lives == 0)
+        {
+            Destroy(gameObject);
+            win.text = "You Lose!";
+        }
     }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.collider.tag == "Ground")
+        if (collision.collider.tag == "Ground" && isOnGround)
         {
             if (Input.GetKey(KeyCode.W))
             {
-                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+                rd2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             }
         }
     }
